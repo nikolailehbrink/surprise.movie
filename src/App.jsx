f9daf62443b52bfd1a32669ee29ab035941c357b
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-20 22:41:15 +0100

    fix: remove unnecessary useEffect
    - Refactor getMovie function to async function outside of useEffect
    - Remove button state

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,57 @@ import { Button } from "./components/ui/button";
 
 function App() {
 	const [movie, setMovie] = useState({});
-	const [nextMovie, setNextMovie] = useState(false);
 	const [movieQuery, setMovieQuery] = useState({
 		"vote_average.gte": 7.2,
 		"vote_count.gte": 200,
 	});
 
 	useEffect(() => {
-		async function getMovie() {
-			try {
-				const response = await fetchMovieDb(`discover/movie`, {
-					query: movieQuery,
-				});
-				const { total_pages: totalPages, total_results: totalResults } =
-					response;
-
-				console.log({ totalPages, totalResults, response });
-
-				const randomPage = Math.floor(Math.random() * (totalPages - 1) + 1);
-
-				const randomResult = () => {
-					let maxResult = 0;
-					if (randomPage !== totalPages) {
-						maxResult = Math.floor(Math.random() * 20);
-					} else {
-						maxResult = Math.floor(Math.random() * (totalResults % 20));
-					}
-					console.log(maxResult);
-					return maxResult;
-				};
-
-				const data = await fetchMovieDb(`discover/movie`, {
-					query: {
-						...movieQuery,
-						page: randomPage,
-					},
-				});
-				console.log({ randomPage, data });
-
-				setMovie(data.results[randomResult()]);
-			} catch (error) {
-				console.log(error);
-			}
-		}
 		getMovie();
-		setNextMovie(false);
-	}, [nextMovie]);
+	}, []);
+
+	async function getMovie() {
+		try {
+			setMovie({});
+			const response = await fetchMovieDb(`discover/movie`, {
+				query: movieQuery,
+			});
+			const { total_pages: totalPages, total_results: totalResults } = response;
+
+			console.log({ totalPages, totalResults, response });
+
+			const randomPage = Math.floor(Math.random() * (totalPages - 1) + 1);
+
+			const randomResult = () => {
+				let maxResult = 0;
+				if (randomPage !== totalPages) {
+					maxResult = Math.floor(Math.random() * 20);
+				} else {
+					maxResult = Math.floor(Math.random() * (totalResults % 20));
+				}
+				console.log(maxResult);
+				return maxResult;
+			};
+
+			const data = await fetchMovieDb(`discover/movie`, {
+				query: {
+					...movieQuery,
+					page: randomPage,
+				},
+			});
+			console.log({ randomPage, data });
+
+			setMovie(data.results[randomResult()]);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<div className="flex flex-col">
+			<Button onClick={getMovie}>Hallo</Button>
 			{Object.keys(movie).length !== 0 ? (
 				<>
-					<Button onClick={() => setNextMovie(true)}>Hallo</Button>
-
 					<Movie movie={movie} />
 				</>
 			) : (
