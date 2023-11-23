2e54a36b803ada99a27a93eda128c101f0329e22
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-23 18:15:54 +0100

    feat: add rating filter
    Fixes #2

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,15 @@
 import { Button } from "./ui/button";
 import GenreFilter from "./GenreFilter";
 import StreamingProviderFilter from "./StreamingProviderFilter";
+import RatingFilter from "./RatingFilter";
 
 export default function Filter({ getMovie }) {
 	return (
 		<div className="flex gap-2 flex-wrap">
 			<StreamingProviderFilter />
 			<GenreFilter />
-			<div className="flex-grow flex items-end justify-end">
+			<div className="flex-grow flex flex-col items-end justify-end">
+				<RatingFilter />
 				<Button className="self-end justify-end" onClick={getMovie}>
 					Surprise me!
 				</Button>
---
+++ src/components/RatingFilter.jsx
@@ -0,0 +1,33 @@
+import { useQueryContext } from "@/App";
+
+export default function RatingFilter() {
+	const ratings = [6.5, 7, 7.5, 8];
+	const { movieQuery, setMovieQuery } = useQueryContext();
+
+	console.log(movieQuery);
+
+	function handleRatingChange(e) {
+		const selectedRating = parseFloat(e.target.value);
+		const query = { ...movieQuery, "vote_average.gte": selectedRating };
+		setMovieQuery(query);
+	}
+	return (
+		<fieldset className="flex items-start flex-wrap border-2 border-white/25 p-3 rounded-3xl max-w-lg">
+			<legend className="px-2 font-bold">Rating:</legend>
+			<select
+				onChange={handleRatingChange}
+				name="rating"
+				id="rating"
+				className="bg-black"
+			>
+				{ratings.map((rating, index) => {
+					return (
+						<option value={rating} key={index} selected={rating === 7}>
+							⭐ {rating} and higher
+						</option>
+					);
+				})}
+			</select>
+		</fieldset>
+	);
+}
