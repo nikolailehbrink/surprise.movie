040e2ff23fbbc8f030c524342eb3d066e0316c56
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-28 16:36:37 +0100

    feat: update logic for watchlist button click

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,22 @@ import { Link } from "wouter";
 import { Button } from "./ui/button";
 import { imageBase } from "@/helpers/movieDb";
 import { Heart } from "@phosphor-icons/react";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function MovieCard({ movie, watchlist, setWatchlist }) {
+	const favored = watchlist.includes(movie);
+	function handleAddToWatchlist() {
+		if (!favored) {
+			setWatchlist([...watchlist, movie]);
+			toast.success("Movie added to watchlist.");
+		} else {
+			const updatedWatchlist = watchlist.filter((item) => item !== movie);
+			setWatchlist(updatedWatchlist);
+			toast.error("Movie removed from watchlist!");
+		}
+	}
 	return (
 		<div className="aspect-[2/3] relative rounded-3xl group overflow-hidden bg-neutral-900 flex items-center justify-center border-2 border-white shadow-2xl shadow-white/30">
 			{Object.keys(movie).length !== 0 ? (
@@ -17,18 +31,16 @@ export default function MovieCard({ movie, watchlist, setWatchlist }) {
 								/10
 							</p>
 							<div className="flex gap-2">
-								<Link href="/movie/:id">
+								<Link href={`/movie/${movie.id}`}>
 									<Button variant="outline" className="text-sm h-full">
 										Details
 									</Button>
 								</Link>
 								<Button
-									onClick={() => {
-										setWatchlist([...watchlist, movie]);
-										console.log(watchlist);
-									}}
+									onClick={handleAddToWatchlist}
 									size="icon"
 									variant="outline"
+									className={cn(favored && "bg-blue")}
 								>
 									<Heart size={24} weight="duotone" />
 								</Button>
