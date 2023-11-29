8f8f80866235401bd6295baf9be2b9bfd3e4acf7
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-29 10:49:20 +0100

    style(MovieCard): rewrite absolute style for div

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,6 @@ import { Button } from "./ui/button";
 import { imageBase } from "@/helpers/movieDb";
 import { Heart } from "@phosphor-icons/react";
 import toast from "react-hot-toast";
-import { useState } from "react";
 import { cn } from "@/lib/utils";
 
 export default function MovieCard({ movie, watchlist, setWatchlist }) {
@@ -22,8 +21,8 @@ export default function MovieCard({ movie, watchlist, setWatchlist }) {
 		<div className="aspect-[2/3] relative rounded-3xl group overflow-hidden bg-neutral-900 flex items-center justify-center border-2 border-white shadow-2xl shadow-white/30">
 			{Object.keys(movie).length !== 0 ? (
 				<>
-					<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 inset-0 top-1/2 bg-gradient-to-t from-black via-black via-50% to-transparent"></div>
-					<div className="absolute sm:translate-y-[calc(100%_+_2rem)] transition-transform duration-500 sm:group-hover:translate-y-0 bottom-4 left-4 right-4 flex gap-3 flex-col">
+					<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 inset-0 top-1/2 bg-gradient-to-t from-black via-black/90 via-50% to-transparent"></div>
+					<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 w-full bottom-0 p-4 flex gap-3 flex-col">
 						<h2 className="font-bold leading-tight ">{movie.title}</h2>
 						<div className="flex gap-2 justify-between items-center flex-wrap">
 							<p className="text-sm">
