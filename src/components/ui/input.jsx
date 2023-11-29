b14cba444441952e4e04a12f96f4a9405114e690
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-29 10:33:19 +0100

    feat: add several ui components for filter section

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+	return (
+		<input
+			type={type}
+			className={cn(
+				"flex w-full rounded-lg border-neutral-500 border-2 bg-neutral-900 px-3 py-2 ring-offset-neutral-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-4 disabled:cursor-not-allowed disabled:opacity-50",
+				className
+			)}
+			ref={ref}
+			{...props}
+		/>
+	);
+});
+Input.displayName = "Input";
+
+export { Input };
