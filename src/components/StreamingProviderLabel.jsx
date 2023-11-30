565385f3364aa4d7b4dd76aa263eb37ec6d3edce
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-30 22:05:30 +0100

    feat(StreamingProviderLabel): add name tooltip

--- src/components/StreamingProviderLabel.jsx
+++ src/components/StreamingProviderLabel.jsx
@@ -1,14 +1,29 @@
 import { imageBase } from "@/helpers/movieDb";
 import { cn } from "@/lib/utils";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "./ui/tooltip";
 
 export default function StreamingProviderLabel({ className, logo, name }) {
 	return (
-		<div className={cn("border-2 p-2 rounded-2xl", className)}>
-			<img
-				className="rounded-lg h-12 flex"
-				src={`${imageBase}/original/${logo}`}
-				alt={`Logo ${name}`}
-			/>
-		</div>
+		<TooltipProvider delayDuration={300}>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<div className={cn("border-2 p-2 rounded-2xl", className)}>
+						<img
+							className="rounded-lg h-12 flex"
+							src={`${imageBase}/original/${logo}`}
+							alt={`Logo ${name}`}
+						/>
+					</div>
+				</TooltipTrigger>
+				<TooltipContent>
+					<p>{name}</p>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
 	);
 }
