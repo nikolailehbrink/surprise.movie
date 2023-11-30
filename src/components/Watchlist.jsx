73c2175ec15d8e0f686d8cad6c3214d87cb18f94
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-30 22:03:04 +0100

    style: add z-index for watchlist cards
    This commit prevents that auto generated QuestionCards are on top of the watchlist entries.

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -50,6 +50,7 @@ export default function Watchlist({ watchlist, setWatchlist }) {
 					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
 						{watchlist.toReversed().map((movie) => (
 							<MovieCard
+								className="z-10"
 								key={movie.id}
 								movie={movie}
 								watchlist={watchlist}
