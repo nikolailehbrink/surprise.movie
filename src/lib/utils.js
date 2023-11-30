b505c58dc77bf04c074618ec280402e041b0104e
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-30 14:33:03 +0100

    refactor: add initials util

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,19 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function initials(name) {
+	const names = name.split(" ");
+	return (
+		names
+			// Only get first and last name
+			.filter((n, i) => i === 0 || i === names.length - 1)
+			// Only get first character
+			.map((n) => n[0])
+			.join("")
+			.toUpperCase()
+	);
 }
