d6076e148bad8d3a1e776d0e8b11b019e99e1cfb
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-11-30 14:37:50 +0100

    fix: make overlay optional and use helpers

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,48 +4,63 @@ import { imageBase } from "@/helpers/movieDb";
 import { Heart } from "@phosphor-icons/react";
 import toast from "react-hot-toast";
 import { cn } from "@/lib/utils";
+import {
+	handleAddToWatchlist,
+	movieInWatchlist,
+} from "@/helpers/watchlistHelper";
 
-export default function MovieCard({ movie, watchlist, setWatchlist }) {
-	const favored = watchlist.includes(movie);
-	function handleAddToWatchlist() {
-		if (!favored) {
-			setWatchlist([...watchlist, movie]);
-			toast.success("Movie added to watchlist.");
-		} else {
-			const updatedWatchlist = watchlist.filter((item) => item !== movie);
-			setWatchlist(updatedWatchlist);
-			toast.error("Movie removed from watchlist!");
-		}
-	}
+export default function MovieCard({
+	className,
+	movie,
+	watchlist,
+	setWatchlist,
+	overlay = true,
+}) {
+	const inWatchlist = movieInWatchlist(watchlist, movie);
+
 	return (
-		<div className="aspect-[2/3] relative rounded-3xl group overflow-hidden bg-neutral-900 flex items-center justify-center border-2 border-white shadow-2xl shadow-white/30">
+		<div
+			className={cn(
+				"aspect-[2/3] relative rounded-3xl group overflow-hidden bg-neutral-900 flex items-center justify-center border-2 border-white shadow-2xl shadow-white/30",
+				className
+			)}
+		>
 			{Object.keys(movie).length !== 0 ? (
 				<>
-					<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 inset-0 top-1/2 bg-gradient-to-t from-black via-black/90 via-50% to-transparent"></div>
-					<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 w-full bottom-0 p-4 flex gap-3 flex-col">
-						<h2 className="font-bold leading-tight ">{movie.title}</h2>
-						<div className="flex gap-2 justify-between items-center flex-wrap">
-							<p className="text-sm">
-								<span className="text-xl font-bold"> {movie.vote_average}</span>
-								/10
-							</p>
-							<div className="flex gap-2">
-								<Link href={`/movie/${movie.id}`}>
-									<Button variant="outline" className="text-sm h-full">
-										Details
-									</Button>
-								</Link>
-								<Button
-									onClick={handleAddToWatchlist}
-									size="icon"
-									variant="outline"
-									className={cn(favored && "bg-blue")}
-								>
-									<Heart size={24} weight="duotone" />
-								</Button>
+					{overlay && (
+						<>
+							<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 inset-0 top-1/2 bg-gradient-to-t from-black via-black/90 via-50% to-transparent"></div>
+							<div className="absolute sm:translate-y-full transition-transform duration-500 sm:group-hover:translate-y-0 w-full bottom-0 p-4 flex gap-3 flex-col">
+								<h2 className="font-bold leading-tight ">{movie.title}</h2>
+								<div className="flex gap-2 justify-between items-center flex-wrap">
+									<p className="text-sm">
+										<span className="text-xl font-bold leading-7">
+											{" "}
+											{movie.vote_average.toFixed(1)}
+										</span>
+										/10
+									</p>
+									<div className="flex gap-2">
+										<Link href={`/movie/${movie.id}`}>
+											<Button variant="outline" className="text-sm h-full">
+												Details
+											</Button>
+										</Link>
+										<Button
+											onClick={() =>
+												handleAddToWatchlist(watchlist, setWatchlist, movie)
+											}
+											size="icon"
+											variant="outline"
+											className={cn(inWatchlist && "bg-blue")}
+										>
+											<Heart size={24} weight="duotone" />
+										</Button>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
+						</>
+					)}
 					<img
 						className="aspect-[inherit] object-cover self-stretch"
 						src={`${imageBase}/w780/${movie.poster_path}`}
