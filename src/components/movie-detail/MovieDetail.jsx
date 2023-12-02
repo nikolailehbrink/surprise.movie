afb2d7e484430ddd45f213d17720266cf7f48c84
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-02 19:41:39 +0100

    feat: add conditional logic for add to watchlist button

--- src/components/movie-detail/MovieDetail.jsx
+++ src/components/movie-detail/MovieDetail.jsx
@@ -56,7 +56,12 @@ export default function MovieDetail({ id, watchlist, setWatchlist }) {
 						</title>
 					</Helmet>
 					<div className="flex max-md:flex-col w-full gap-4 xl:gap-12">
-						<div className="md:w-1/4 max-md:z-10 shrink-0 flex flex-col gap-3 sticky top-20 md:self-start">
+						<div
+							className={cn(
+								"md:w-1/4 max-md:z-30 shrink-0 flex flex-col gap-3 md:self-start",
+								!inWatchlist && "sticky top-[84px]"
+							)}
+						>
 							<MovieCard
 								className="hidden md:flex"
 								overlay={false}
