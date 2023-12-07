f57afc8334eb8271797163828adcfaa3ab5284b0
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-07 16:42:22 +0100

    feat: update spinner icon

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useQueryContext } from "@/App";
 import { fetchMovieDb } from "@/helpers/movieDb";
-import { Popcorn } from "@phosphor-icons/react";
+import { Popcorn, Spinner } from "@phosphor-icons/react";
 import { useEffect, useRef, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import toast from "react-hot-toast";
@@ -61,28 +61,7 @@ export default function Home() {
 						onClick={getMovie}
 					>
 						{imageLoading ? (
-							<div className="h-8 w-8 flex items-center justify-center">
-								<svg
-									className="animate-spin h-[26px] w-[26px] text-white"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-								>
-									<circle
-										className="opacity-25"
-										cx="12"
-										cy="12"
-										r="10"
-										stroke="currentColor"
-										strokeWidth="4"
-									></circle>
-									<path
-										className="opacity-75"
-										fill="currentColor"
-										d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-									></path>
-								</svg>
-							</div>
+							<Spinner className="animate-spin" weight="duotone" size={32} />
 						) : (
 							<Popcorn size={32} weight="duotone" />
 						)}
