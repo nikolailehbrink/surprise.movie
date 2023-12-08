c8943a7d22cc412a994fb78e088c942b46923f23
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-08 22:32:00 +0100

    chore: add svgr to project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
