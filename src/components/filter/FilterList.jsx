456024fad08b5fff0b186d522f8e511727afa3df
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-08 23:20:00 +0100

    feat: add FilterPopover component

--- src/components/filter/FilterList.jsx
+++ src/components/filter/FilterList.jsx
@@ -1,10 +1,8 @@
 import { getCountryCode } from "@/helpers/languageHelper";
 import { fetchMovieDb } from "@/helpers/movieDb";
 import { cn } from "@/lib/utils";
-import { Calendar, FilmStrip, Monitor, StarHalf } from "@phosphor-icons/react";
+import { Calendar, FilmStrip, Monitor, Star } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
-import { Button } from "../ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { ScrollArea } from "../ui/scroll-area";
 import GenreFilter from "./GenreFilter";
 import RatingFilter from "./RatingFilter";
@@ -12,6 +10,7 @@ import StreamingProviderFilter from "./StreamingProviderFilter";
 import YearFilter from "./YearFilter";
 import { isCorrectYearInput } from "@/helpers/yearInput";
 import { useQueryContext } from "@/context/QueryContext";
+import FilterPopover from "./FilterPopover";
 
 export default function FilterList({ className }) {
 	const { movieQuery, setMovieQuery } = useQueryContext();
@@ -222,100 +221,58 @@ export default function FilterList({ className }) {
 
 	return (
 		<div className={cn("flex gap-4 flex-wrap justify-center", className)}>
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						className={cn(
-							selectedProvider.length > 0 ? "bg-neutral-800" : "opacity-80"
-						)}
-						variant="outline"
-						size="lg"
-					>
-						<Monitor size={32} weight="duotone" />
-						Streaming
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent>
-					<StreamingProviderFilter
-						providers={providers}
-						selectedProvider={selectedProvider}
-						setSelectedProvider={setSelectedProvider}
-					/>
-				</PopoverContent>
-			</Popover>
+			<FilterPopover
+				isSelected={selectedProvider.length > 0}
+				icon={<Monitor size={32} weight="duotone" />}
+				text="Streaming"
+			>
+				<StreamingProviderFilter
+					providers={providers}
+					selectedProvider={selectedProvider}
+					setSelectedProvider={setSelectedProvider}
+				/>
+			</FilterPopover>
 
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						className={cn(
-							beginningYear !== minimumYear || endYear !== currentYear
-								? "bg-neutral-800"
-								: "opacity-80"
-						)}
-						variant="outline"
-						size="lg"
-					>
-						<Calendar size={32} weight="duotone" />
-						Year
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent className="max-w-[280px] sm:max-w-xs">
-					<YearFilter
-						beginningYear={beginningYear}
-						setBeginningYear={setBeginningYear}
-						endYear={endYear}
-						setEndYear={setEndYear}
-						minimumYear={minimumYear}
-						currentYear={currentYear}
-					/>
-				</PopoverContent>
-			</Popover>
+			<FilterPopover
+				isSelected={beginningYear !== minimumYear || endYear !== currentYear}
+				icon={<Calendar size={32} weight="duotone" />}
+				text="Year"
+			>
+				<YearFilter
+					beginningYear={beginningYear}
+					setBeginningYear={setBeginningYear}
+					endYear={endYear}
+					setEndYear={setEndYear}
+					minimumYear={minimumYear}
+					currentYear={currentYear}
+				/>
+			</FilterPopover>
 
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						className={cn(
-							selectedRating !== 7 ? "bg-neutral-800" : "opacity-80"
-						)}
-						variant="outline"
-						size="lg"
-					>
-						<StarHalf size={32} weight="duotone" />
-						Rating
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent>
-					<RatingFilter
-						ratings={ratings}
-						selectedRating={selectedRating}
-						setSelectedRating={setSelectedRating}
-					/>
-				</PopoverContent>
-			</Popover>
+			<FilterPopover
+				isSelected={selectedRating !== 7}
+				icon={<Star size={32} weight="duotone" />}
+				text="Rating"
+			>
+				<RatingFilter
+					ratings={ratings}
+					selectedRating={selectedRating}
+					setSelectedRating={setSelectedRating}
+				/>
+			</FilterPopover>
 
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						className={cn(
-							selectedGenres.length > 0 ? "bg-neutral-800" : "opacity-80"
-						)}
-						variant="outline"
-						size="lg"
-					>
-						<FilmStrip size={32} weight="duotone" />
-						Genres
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent>
-					<ScrollArea className="h-[20rem]">
-						<GenreFilter
-							genres={genres}
-							selectedGenres={selectedGenres}
-							setSelectedGenres={setSelectedGenres}
-						/>
-					</ScrollArea>
-				</PopoverContent>
-			</Popover>
+			<FilterPopover
+				isSelected={selectedGenres.length > 0}
+				icon={<FilmStrip size={32} weight="duotone" />}
+				text="Genres"
+			>
+				<ScrollArea className="h-[20rem]">
+					<GenreFilter
+						genres={genres}
+						selectedGenres={selectedGenres}
+						setSelectedGenres={setSelectedGenres}
+					/>
+				</ScrollArea>
+			</FilterPopover>
 		</div>
 	);
 }
---
+++ src/components/filter/FilterPopover.jsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+export default function FilterPopover({ isSelected, icon, text, children }) {
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					className={cn(isSelected ? "bg-neutral-800" : "opacity-80")}
+					variant="outline"
+					size="lg"
+				>
+					{icon}
+					{text}
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="max-w-[280px] sm:max-w-xs">
+				{children}
+			</PopoverContent>
+		</Popover>
+	);
+}
