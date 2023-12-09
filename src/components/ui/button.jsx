03b8af9538b2975e042396f45c23db1afbab8d25
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-09 22:47:02 +0100

    style(Button): add background-color for active state

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
 		variants: {
 			variant: {
 				default:
-					"bg-blue border-white border-2 text-neutral-50 hover:bg-blue-400 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
+					"bg-blue active:bg-blue border-white border-2 text-neutral-50 hover:bg-blue-400 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
 				destructive:
 					"bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
 				outline: "border border-white border-2 hover:bg-neutral-800",
