e799e3020b44d22a3415116994e7445001fa364e
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-26 00:05:06 +0100

    feat: add inter font

--- app/root.tsx
+++ app/root.tsx
@@ -1,40 +1,38 @@
-import { LinksFunction } from '@remix-run/node'
+import { LinksFunction } from "@remix-run/node";
 import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-} from '@remix-run/react'
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import "@fontsource-variable/inter";
 
-import tailwind from '~/globals.css?url'
+import tailwind from "~/globals.css?url";
 
 export const links: LinksFunction = () => [
-    { rel: 'stylesheet', href: tailwind },
-    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-]
+  { rel: "stylesheet", href: tailwind },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en" className="text-background bg-foreground">
-            <head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                {children}
-                <ScrollRestoration />
-                <Scripts />
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en" className="bg-foreground font-inter text-background">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-    return <Outlet />
+  return <Outlet />;
 }
