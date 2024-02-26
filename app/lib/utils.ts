844af21fe1ce08820078f6a1bf45ac6bd9612c39
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-26 16:52:40 +0100

    feat: add copy to clipboard util

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export async function copyToClipboard(
+  text: string,
+  {
+    options: { success, error } = { success: undefined, error: undefined },
+  }: {
+    options?: {
+      success?: () => void;
+      error?: (e?: unknown) => void;
+    };
+  } = {},
+) {
+  try {
+    await navigator.clipboard.writeText(text);
+    if (success) {
+      success();
+    }
+  } catch (e) {
+    if (error) {
+      error(e);
+    }
+  }
+}
