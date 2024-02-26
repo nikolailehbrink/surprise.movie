5039565efd0393f0d8a57c8f4bbd3ac9ea14c2d9
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-26 12:03:22 +0100

    feat: add vercel speed insights

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 import "@fontsource-variable/inter";
 
 import styles from "@/globals.css?url";
+import { SpeedInsights } from "@vercel/speed-insights/remix";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -31,6 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <SpeedInsights />
       </body>
     </html>
   );
