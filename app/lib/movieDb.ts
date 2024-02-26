59a6e1288c92de81e2fc309195d6b1ab75c097ba
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-26 22:32:29 +0100

    feat: implement random movie loader

--- app/lib/movieDb.ts
+++ app/lib/movieDb.ts
@@ -23,3 +23,50 @@ export const fetchTMDB = (
     },
   });
 };
+export const getRandomPage = async () => {
+  const response = await fetchTMDB(`discover/movie`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data from TMDB");
+  }
+
+  const data = (await response.json()) as DiscoverMovie;
+
+  const { total_pages: totalPages, total_results: totalResults } = data;
+
+  if (totalResults === 0) {
+    throw new Error("No movies found");
+  }
+
+  const randomPage = Math.floor(
+    Math.random() * (totalPages > 500 ? 500 : totalPages - 1 + 1),
+  );
+
+  // If the random page is the last page, it can have less than 20 results
+  const randomResult = Math.floor(
+    Math.random() *
+      (randomPage === totalPages && totalResults % 20 !== 0
+        ? totalResults % 20
+        : 20),
+  );
+
+  console.log(randomPage, randomResult);
+
+  return { randomPage, randomResult };
+};
+
+export const getRandomMovie = async (page: number, result: number) => {
+  const response = await fetchTMDB(`discover/movie`, {
+    searchParams: { page: page.toString() },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data from TMDB");
+  }
+
+  const { results } = (await response.json()) as DiscoverMovie;
+
+  const movie = results[result];
+
+  return movie;
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,34 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { getRandomMovie, getRandomPage } from "@/lib/movieDb";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const { randomPage, randomResult } = await getRandomPage();
+
+  const movie = await getRandomMovie(randomPage, randomResult);
+
+  return json({ movie });
+};
+
 export default function Index() {
+  const { movie } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1 className="text-wrap text-4xl font-bold text-sky-500">
         Welcome to Remix
       </h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <pre className="overflow-auto">{JSON.stringify(movie, null, 2)}</pre>
     </div>
   );
 }
