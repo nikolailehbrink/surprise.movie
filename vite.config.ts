41088d959c08e43372a138d780b898eb93ffa138
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-26 01:08:45 +0100

    chore: use vite-tsconfig-paths plugin for import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import svgr from "vite-plugin-svgr";
 import { vitePlugin as remix } from "@remix-run/dev";
-import path from "path";
 import { defineConfig } from "vite";
+// Give vite the ability to resolve imports using TypeScript's path mapping, which is done in tsconfig.json
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "app"),
-    },
-  },
-  plugins: [remix(), tsconfigPaths()],
   plugins: [remix(), tsconfigPaths(), svgr()],
 });
