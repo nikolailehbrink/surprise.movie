3b5f3e9915df84817884dc9d5c53614bc31bcdcf
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-03-13 15:47:24 +0100

    feat: add movie card and add persistent movie state

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
 import Filter from "@/components/filter";
 import GradientHeading from "@/components/gradient-heading";
+import MovieCard from "@/components/movie-card";
+import MovieCardOverlay from "@/components/movie-card-overlay";
+import QuestionCard from "@/components/question-card";
 import { Button } from "@/components/ui/button";
+import { movieAtom } from "@/lib/atoms";
 import {
   getMovieGenres,
   getRandomMovie,
@@ -8,7 +12,9 @@ import {
 } from "@/lib/movie-database";
 import { CircleNotch, Popcorn } from "@phosphor-icons/react";
 import { ActionFunctionArgs } from "@remix-run/node";
-import { Link, json, useFetcher, useLoaderData } from "@remix-run/react";
+import { json, useFetcher, useLoaderData } from "@remix-run/react";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 export const loader = async () => {
   const [{ results: streamingProviders }, genres] = await Promise.all([
@@ -25,40 +31,56 @@ export default function Index() {
 
   const isLoading = state === "submitting";
 
+  const [movie, setMovie] = useAtom(movieAtom);
+
+  useEffect(() => {
+    if (data?.movie) setMovie(data.movie);
+  }, [data?.movie, setMovie]);
+
   return (
     <div className="container relative flex flex-grow flex-col justify-center gap-4 py-12 sm:py-16">
       <GradientHeading className="mb-12">
         Discover your next favorite movie
       </GradientHeading>
-      <div>
-        {data && (
-          <>
-            <pre className="overflow-auto">
-              {JSON.stringify(data.movie, null, 2)}
-            </pre>
-            <Button asChild>
-              <Link to={`/movie/${data.movie.id}`}>Go</Link>
-            </Button>
-          </>
+      <div className="mx-auto grid w-full grid-cols-1 items-center justify-center gap-4 max-sm:max-w-[20rem] sm:grid-cols-3 sm:self-stretch lg:grid-cols-5">
+        <QuestionCard className="hidden lg:flex lg:h-[90%] lg:justify-self-end" />
+        <QuestionCard
+          delay={500}
+          className="hidden h-[95%] justify-self-center sm:flex"
+        />
+
+        {!isLoading && movie !== null ? (
+          <MovieCard
+            className="border-2 border-white shadow-2xl shadow-white/30"
+            movie={movie}
+          >
+            <MovieCardOverlay movie={movie} />
+          </MovieCard>
+        ) : (
+          <QuestionCard
+            delay={1000}
+            className="bg-neutral-700 text-neutral-500"
+          />
         )}
-
-        <Filter streamingProviders={streamingProviders} genres={genres} />
-
-        <Form method="post">
-          <Button disabled={isLoading}>
-            {isLoading ? (
-              <CircleNotch
-                className="animate-spin"
-                weight="duotone"
-                size={24}
-              />
-            ) : (
-              <Popcorn size={24} weight="duotone" />
-            )}
-            Surprise me!
-          </Button>
-        </Form>
+        <QuestionCard
+          delay={1500}
+          className="hidden h-[95%] justify-self-center sm:flex"
+        />
+        <QuestionCard delay={2000} className="hidden lg:flex lg:h-[90%]" />
       </div>
+      <Filter streamingProviders={streamingProviders} genres={genres} />
+
+      <Form className="sticky bottom-0 z-50 flex justify-center" method="post">
+        <div className="absolute inset-0 -inset-x-4 h-40 bg-gradient-to-t from-transparent via-[#060606] to-transparent md:hidden"></div>
+        <Button className="relative mb-4 self-center" disabled={isLoading}>
+          {isLoading ? (
+            <CircleNotch className="animate-spin" weight="duotone" size={24} />
+          ) : (
+            <Popcorn size={24} weight="duotone" />
+          )}
+          Surprise me!
+        </Button>
+      </Form>
     </div>
   );
 }
