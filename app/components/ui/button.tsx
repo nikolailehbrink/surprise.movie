9882a4d01c26fc34b40371b953ea2dc1c886bf54
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-03-13 15:49:34 +0100

    style(Button): update default colors

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-sky-500 text-primary border-2 border-foreground hover:bg-sky-500/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
